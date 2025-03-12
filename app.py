96e7fa9fe2fda92efd85c97f74739903010e695f
Gagan Yadav <gaganiittm5@gmail.com>
2025-03-12 14:04:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello():
-#     return 'Hello, LOCO!\n'
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=80)
-
 from flask import Flask, render_template, jsonify
 import datetime
 import random
